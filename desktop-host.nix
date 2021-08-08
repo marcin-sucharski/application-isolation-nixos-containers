8a8e1560908fdf8d045090cb27dd6ae2d65667be
Marcin Sucharski <marcin.sucharski@off-by.one>
2021-08-09 00:47:31 +0200

    Container with graphical applications

--- desktop-host.nix
+++ desktop-host.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   home-manager = builtins.fetchGit {
@@ -19,23 +19,58 @@ in {
     };
   };
 
-  containers.cliExample = let
+  containers.graphicalExample = let
+    hostCfg = config;
     userName = "myuser";
+    userUid = hostCfg.users.users."${userName}".uid;
   in {
+    bindMounts = {
+      waylandDisplay = rec {
+        hostPath = "/run/user/${toString userUid}";
+        mountPoint = hostPath;
+      };
+      x11Display = rec {
+        hostPath = "/tmp/.X11-unix";
+        mountPoint = hostPath;
+        isReadOnly = true;
+      };
+    };
+
     config = {
       imports = [
         (import "${home-manager}/nixos")
         ./desktop.nix
       ];
 
+      hardware.opengl = {
+        enable = true;
+        extraPackages = hostCfg.hardware.opengl.extraPackages;
+      };
+
       users.users."${userName}".extraGroups = lib.mkForce [];
 
-      environment.systemPackages = with pkgs; [ vim ];
+      environment.systemPackages = with pkgs; [
+        vim
+        jetbrains.idea-community
+      ];
 
       home-manager = {
         useGlobalPkgs = true;
         users.myuser = {
           imports = [ ./home.nix ];
+
+          home.sessionVariables = {
+            WAYLAND_DISPLAY                     = "wayland-1";
+            QT_QPA_PLATFORM                     = "wayland";
+            QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+            SDL_VIDEODRIVER                     = "wayland";
+            CLUTTER_BACKEND                     = "wayland";
+            MOZ_ENABLE_WAYLAND                  = "1";
+            _JAVA_AWT_WM_NONREPARENTING         = "1";
+            _JAVA_OPTIONS                       = "-Dawt.useSystemAAFontSettings=lcd";
+            XDG_RUNTIME_DIR                     = "/run/user/${toString userUid}";
+            DISPLAY                             = ":0";
+          };
         };
       };
 
